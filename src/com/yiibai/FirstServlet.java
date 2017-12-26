bc658f6dbec2ec00c3150eefd5913b1ef54fb4c4
Vladimir-Bayes <gyang.shines@gmail.com>
2017-12-26 17:25:51 +0800

    Initial commit

---
+++ src/com/yiibai/FirstServlet.java
@@ -0,0 +1,37 @@
+package com.yiibai;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		String n = request.getParameter("username");
+
+		out.print("您好， " + n + "<br/><hr/>");
+
+		// appending the username in the query string
+		out.print("<a href='servlet2?uname=" + n + "'>使用URL参数访问用户名</a>");
+
+		out.close();
+	}
+
+}
